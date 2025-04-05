6f8c50d57fd84d3637c0cf90e9fb10eb7cad28e0
Jannis Icken <jannis.icken@amazone.de>
2025-04-05 10:57:12 +0200

    added test for conversion of TimeLog-record time into Date-object

--- src/entities/TimeLog/TimeLog.spec.ts
+++ src/entities/TimeLog/TimeLog.spec.ts
@@ -18,6 +18,19 @@ describe('TimeLog Entity', () => {
         expect(parsedTimeLog.bbox).toEqual([9.5777866, 45.5277534, 9.5779409, 45.5278066])
     })
 
+    it('should convert TimeLog-record times into correct Date-objects', async () => {
+        const isoxmlData = readFileSync('./data/2021-04-09T15_33_26_taskdata.zip')
+        const isoxmlManager = new ISOXMLManager({version: 4})
+
+        await isoxmlManager.parseISOXMLFile(new Uint8Array(isoxmlData.buffer), 'application/zip')
+
+        const timeLog = isoxmlManager.getEntityByXmlId<Task>('TSK-1').attributes.TimeLog[0] as ExtendedTimeLog
+
+        const parsedTimeLog = timeLog.parseBinaryFile()
+
+        expect(parsedTimeLog.timeLogs[0].time.toUTCString()).toEqual('Fri, 09 Apr 2021 14:54:04 GMT')
+    })
+
     it('should detect outliers', async () => {
         const isoxmlData = readFileSync('./data/2021-04-09T15_33_26_taskdata.zip')
         const isoxmlManager = new ISOXMLManager()
@@ -31,6 +44,7 @@ describe('TimeLog Entity', () => {
         expect(ranges[2].minValue).toBe(-2231)
         expect(ranges[2].maxValue).toBe(334)
     })
+
 
     it('should fill missing values', async () => {
         const isoxmlData = readFileSync('./data/2021-04-09T15_33_26_taskdata.zip')
